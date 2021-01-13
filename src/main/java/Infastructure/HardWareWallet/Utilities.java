10ab9e924a559d83a4e5fe42951d67d590849e8c
Daniel Schaefer <46212174+Daniel-SchaeferJ@users.noreply.github.com>
2021-01-12 16:41:27 -0800

    Adding utilities folder

---
+++ src/main/java/Infastructure/HardWareWallet/Utilities.java
@@ -0,0 +1,10 @@
+package Infastructure.HardWareWallet;
+import org.basex.util.Base64;
+
+public class Utilities {
+
+    public static final String BASE58_ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+    public  static  final String =
+    public static final String BECH32_ALPHABET = "qpzry9x8gf2tvdw0s3jn54khce6mua7l";
+
+}
